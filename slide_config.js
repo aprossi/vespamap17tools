f068ef7b8459bd291772610644bf5d0d33abf4cb
aprossi <aprossi@gmail.com>
2017-04-20 10:10:53 +0200

    added tool/services slides from vespa mapping workshop

---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Planetary data & mapping',
+    subtitle: 'Web services and tools',
+    // eventInfo: {
+    //  title: '       #vespamap17',
+    //  date: '19/04/2017'
+    // },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    // favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    // theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Angelo Pio Rossi',
+    company: 'Jacobs University Bremen <img src="images/logo-jacobs.svg" height="32px">',
+    // gplus: 'http://plus.google.com/1234567890',
+    twitter: '@arosp',
+    www: 'http://aprossi.eu',
+    github: 'http://github.com/aprossi'
+  }, {
+    name: 'Trent Hare',
+    company: 'USGS <img src="images/logo-usgs.svg" height="32px">',
+    // gplus: 'http://plus.google.com/1234567890',
+    // twitter: '@yourhandle',
+    // www: 'http://www.you.com',
+    github: 'http://github.com/THareUSGS'
+  }]
+};
+
